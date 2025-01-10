b1626c45e3dde879ea6d2aeaaf2aab40379b9494
Transvolt-gith <transvolttechnologies@gmail.com>
2025-01-10 11:46:11 +0530

    Update contact.service.ts

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,7 +8,7 @@ import { Contact } from './contact';
 })
 export class ContactService {
 
-  private baseUrl="http://ab5d88d3713894ac1a1177ce76b11877-1673811767.us-west-2.elb.amazonaws.com/"
+  private baseUrl="https://a037837cf98d740fb937809ea765c980-297818421.us-east-1.elb.amazonaws.com/"
 
   constructor(private httpClient:HttpClient) { }
 
